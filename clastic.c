4906b265c42790d9d2fa677775598df244ee9512
Phuong Do <dongphuong2004@gmail.com>
2017-12-01 16:42:40 +0900

    Create API for clastic

---
+++ clastic.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "clastic.h"
+
+
+struct _clastic_t {
+};
+
+clastic_t *clastic_init(const char *host)
+{
+    //TODO
+    return NULL;
+}
+
+void clastic_destroy(clastic_t *cls)
+{
+    //TODO
+}
+
+int clastic_index_exist(clastic_t *cls, const char *index)
+{
+    return 0;
+    //TODO
+}
+
+int clastic_create_index(clastic_t *cls, const char *index)
+{
+    return 0;
+    //TODO
+}
+
+int clastic_delete_index(clastic_t *cls, const char *index)
+{
+    return 0;
+    //TODO
+}
+
+void clastic_get_by_id(clastic_t *cls, const char *index, const char *type, const char *id, char *data)
+{
+    //TODO
+}
+
+void clastic_get_by_query(clastic_t *cls, const char *index, const char *type, const char *query, char *data)
+{
+    //TODO
+}
+
+void clastic_put(clastic_t *cls, const char *index, const char *type, const char *id, char *data)
+{
+    //TODO
+}
+
+int clastic_count(clastic_t *cls, const char *index, const char *type)
+{
+    return 0;
+    //TODO
+}
+
+#endif
---
+++ clastic.h
@@ -0,0 +1,77 @@
+#ifndef __CLASTIC_H__
+#define __CLASTIC_H__
+
+typedef struct _clastic_t clastic_t;
+
+/**
+  * @brief Init and return handler to clastic
+  * @param host Elasticsearch server path
+  * @return pointer to clastic_t
+  */
+clastic_t *clastic_init(const char *host);
+
+/**
+  * @brief Destroy
+  */
+void clastic_destroy(clastic_t *cls);
+
+/**
+  * @brief Check if index exist
+  * @param cls Pointer to clastic_t
+  * @param index Index string
+  * @return 1 if index exists, or else return 0
+  */
+int clastic_index_exist(clastic_t *cls, const char *index);
+
+/**
+  * @brief Create new index
+  * @param cls Pointer to clastic_t
+  * @param index Index to be created
+  * @return 0 if success
+  */
+int clastic_create_index(clastic_t *cls, const char *index);
+
+/**
+  * @brief Delete an index
+  * @param cls Pointer to clastic_t
+  * @param index Index to be deleted
+  * @return 0 if success
+  */
+int clastic_delete_index(clastic_t *cls, const char *index);
+
+/**
+  * @brief Get and index from id
+  * @param[in] index Index
+  * @param[in] type Type
+  * @param[in] id id
+  * @param[out] data Found data
+  */
+void clastic_get_by_id(clastic_t *cls, const char *index, const char *type, const char *id, char *data);
+
+/**
+  * @brief Get and index from query
+  * @param[in] index Index
+  * @param[in] type Type
+  * @param[in] id id
+  * @param[out] data Found data (can be multiple record)
+  */
+void clastic_get_by_query(clastic_t *cls, const char *index, const char *type, const char *query, char *data);
+
+/**
+  * @brief Insert data into an index
+  * @param[in] index Index
+  * @param[in] type Type
+  * @param[in] id id
+  * @param[in] data Found data
+  */
+void clastic_put(clastic_t *cls, const char *index, const char *type, const char *id, char *data);
+
+/**
+  * @brief Count number of documents
+  * @param[in] index Index
+  * @param[in] type Type
+  * @return Number of documents
+  */
+int clastic_count(clastic_t *cls, const char *index, const char *type);
+
+#endif
